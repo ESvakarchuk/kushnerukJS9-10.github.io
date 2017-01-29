b75d77d5b4087e6aa416e8fb249bac19516069b9
ESvakarchuk <e.s.vakarchuk@gmail.com>
2017-01-29 16:14:00 +0200

    Second commit

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(function() {
     $('.jcarousel').jcarousel({
         // Configuration goes here
     });
+
+    $('.jcarousel-prev').click(function() {
+      $('.jcarousel').jcarousel('scroll', '-=1');
+    });
+
+    $('.jcarousel-next').click(function() {
+      $('.jcarousel').jcarousel('scroll', '+=1');
+    });
 });
