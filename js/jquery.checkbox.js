b14ab58dbff166174bf78eafbba2959d8b62ec49
ESvakarchuk <e.s.vakarchuk@gmail.com>
2017-01-31 04:39:45 +0200

    Third commit

---
+++ js/jquery.checkbox.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function() {
+
+    jQuery(".niceCheck").mousedown(function() {
+
+        changeCheck(jQuery(this));
+
+    });
+
+
+    jQuery(".niceCheck").each(function() {
+
+        changeCheckStart(jQuery(this));
+
+    });
+
+});
+
+function changeCheck(el) {
+    input = el.find("input").eq(0);
+    if (!input.attr("checked")) {
+        el.css("background-position", "0 -17px");
+        input.attr("checked", true);
+    } else {
+        el.css("background-position", "0 0");
+        input.attr("checked", false);
+    }
+    return true;
+}
+
+function changeCheckStart(el) {
+    input = el.find("input").eq(0);
+    if (input.attr("checked")) {
+        el.css("background-position", "0 -17px");
+    }
+    return true;
+}
--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
 $(function() {
+
+    // JCarousel
+
     $('.jcarousel').jcarousel({
         // Configuration goes here
     });
 
     $('.jcarousel-prev').click(function() {
-      $('.jcarousel').jcarousel('scroll', '-=1');
+        $('.jcarousel').jcarousel('scroll', '-=1');
     });
 
     $('.jcarousel-next').click(function() {
-      $('.jcarousel').jcarousel('scroll', '+=1');
+        $('.jcarousel').jcarousel('scroll', '+=1');
     });
+
+    // Select box
+
+    $(".my_select_box").chosen({
+        disable_search_threshold: 10,
+        no_results_text: "Oops, nothing found!",
+        width: "50%",
+    });
+
+    // Nav menu
+
+    $('.main-menu').hover(function() {
+            $(this).children('.sub-menu').stop(true, true);
+            $(this).children('.sub-menu').slideDown(600).animate({
+                backgroundColor: '#045F94'
+            });
+        },
+
+        function() {
+            $(this).children('.sub-menu').delay(200).slideUp(300).animate({
+                backgroundColor: '#258dc8'
+            });
+        }
+    );
+
+
 });
